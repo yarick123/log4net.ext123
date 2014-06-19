7809d38d3de88a69647271350cbd6eea849aedee
yarick123 <yarick123@users.noreply.github.com>
2014-06-19 14:44:10 +0200

    refactoring: introduced method getLogRepo2Test

--- log4net.ext123-test/Log123_test.cs
+++ log4net.ext123-test/Log123_test.cs
@@ -34,15 +34,20 @@ namespace log4net.ext123
 				</logger>
 			</log4net>";
 
-		[Test]
-		public void Test_LogEnabled() {
+		private string getLogRepo2Test() {
 			var log4netConfig = new XmlDocument();
 			log4netConfig.LoadXml(TEST_LOG4NET_CONF);
 
 			ILoggerRepository rep = LogManager.CreateRepository(Guid.NewGuid().ToString());
 			XmlConfigurator.Configure(rep, log4netConfig["log4net"]);
 
-			ILog123 a = Log123Manager.GetLogger(rep.Name, "a");
+			return rep.Name;
+		}
+
+		[Test]
+		public void Test_LogEnabled() {
+			string repoName = getLogRepo2Test();
+			ILog123 a = Log123Manager.GetLogger(repoName, "a");
 
 			Assert.IsTrue(a.IsTraceEnabled);
 			Assert.IsTrue(a.IsDebugEnabled);
@@ -51,7 +56,7 @@ namespace log4net.ext123
 			Assert.IsTrue(a.IsErrorEnabled);
 			Assert.IsTrue(a.IsFatalEnabled);
 
-			ILog123 w = Log123Manager.GetLogger(rep.Name, "W");
+			ILog123 w = Log123Manager.GetLogger(repoName, "W");
 			Assert.IsFalse(w.IsTraceEnabled);
 			Assert.IsFalse(w.IsDebugEnabled);
 			Assert.IsFalse(w.IsInfoEnabled);
@@ -62,19 +67,14 @@ namespace log4net.ext123
 
 		[Test]
 		public void Test_LogMessage() {
-			var log4netConfig = new XmlDocument();
-			log4netConfig.LoadXml(TEST_LOG4NET_CONF);
-
-			ILoggerRepository rep = LogManager.CreateRepository(Guid.NewGuid().ToString());
-			XmlConfigurator.Configure(rep, log4netConfig["log4net"]);
-
-			ILog123 log = Log123Manager.GetLogger(rep.Name, "a");
+			string repoName = getLogRepo2Test();
+			ILog123 log = Log123Manager.GetLogger(repoName, "a");
 
 			log.Trace("trace");
 			log.Debug("Message");
 			log.Info(55);
 
-			var appender = (StringAppender)LogManager.GetRepository(rep.Name).GetAppenders()[0];
+			var appender = (StringAppender)LogManager.GetRepository(repoName).GetAppenders()[0];
 			Assert.AreEqual("TRACE - trace;DEBUG - Message;INFO - 55;", appender.GetString());
 			appender.Reset();
 
