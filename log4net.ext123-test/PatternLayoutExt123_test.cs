1069c13ebf74dfb835b599c78316131245f0e7c5
yarick123 <yarick123@users.noreply.github.com>
2014-06-25 10:30:39 +0200

    added optional option {Length} to the pattern %short-level

--- log4net.ext123-test/PatternLayoutExt123_test.cs
+++ log4net.ext123-test/PatternLayoutExt123_test.cs
@@ -23,9 +23,9 @@ namespace log4net.ext123
 				</root>
 			</log4net>";
 
-		private string getLogRepo2Test() {
+		private string getLogRepo2Test(string log4net_config=TEST_LOG4NET_CONF) {
 			var log4netConfig = new XmlDocument();
-			log4netConfig.LoadXml(TEST_LOG4NET_CONF);
+			log4netConfig.LoadXml(log4net_config);
 
 			ILoggerRepository rep = LogManager.CreateRepository(Guid.NewGuid().ToString());
 			XmlConfigurator.Configure(rep, log4netConfig["log4net"]);
@@ -47,6 +47,59 @@ namespace log4net.ext123
 
 			var appender = (StringAppender)LogManager.GetRepository(repoName).GetAppenders()[0];
 			Assert.AreEqual("T: trace;D: Message;I: 55;W: 1;E: 2;F: 3;", appender.GetString());
+		}
+
+		[Test]
+		public void Test_PatternLayoutExt123_Length() {
+			string configTemplate= @"
+			<log4net>
+				<appender name='a' type='log4net.Tests.Appender.StringAppender'>
+					<layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level: %message;'/></layout>
+				</appender>
+				<appender name='a0' type='log4net.Tests.Appender.StringAppender'>
+					<layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level{0}: %message;'/></layout>
+				</appender>
+				<appender name='a1' type='log4net.Tests.Appender.StringAppender'>
+					<layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level{1}: %message;'/></layout>
+				</appender>
+				<appender name='a2' type='log4net.Tests.Appender.StringAppender'>
+					<layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level{2}: %message;'/></layout>
+				</appender>
+				<appender name='a6' type='log4net.Tests.Appender.StringAppender'>
+					<layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level{6}: %message;'/></layout>
+				</appender>
+				<root>
+					<level value='ALL' />
+					<appender-ref ref='a' />
+					<appender-ref ref='a0' />
+					<appender-ref ref='a1' />
+					<appender-ref ref='a2' />
+					<appender-ref ref='a6' />
+				</root>
+			</log4net>";
+
+			
+			string repoName = getLogRepo2Test(configTemplate);
+			ILog123 log = Log123Manager.GetLogger(repoName, "root");
+
+			log.Trace("trace");
+			log.Debug("Message");
+			log.Info(55);
+			log.Warn(1);
+			log.Error(2);
+			log.Fatal(3);
+
+			var a = (StringAppender) LogManager.GetRepository(repoName).GetAppenders()[0];
+			var a0 = (StringAppender) LogManager.GetRepository(repoName).GetAppenders()[1];
+			var a1 = (StringAppender) LogManager.GetRepository(repoName).GetAppenders()[2];
+			var a2 = (StringAppender) LogManager.GetRepository(repoName).GetAppenders()[3];
+			var a6 = (StringAppender) LogManager.GetRepository(repoName).GetAppenders()[4];
+
+			Assert.AreEqual("T: trace;D: Message;I: 55;W: 1;E: 2;F: 3;", a.GetString());
+			Assert.AreEqual(": trace;: Message;: 55;: 1;: 2;: 3;", a0.GetString());
+			Assert.AreEqual("T: trace;D: Message;I: 55;W: 1;E: 2;F: 3;", a1.GetString());
+			Assert.AreEqual("TR: trace;DE: Message;IN: 55;WA: 1;ER: 2;FA: 3;", a2.GetString());
+			Assert.AreEqual("TRACE : trace;DEBUG : Message;INFO  : 55;WARN  : 1;ERROR : 2;FATAL : 3;", a6.GetString());
 		}
 	}
 }
--- log4net.ext123/PatternLayoutExt123.cs
+++ log4net.ext123/PatternLayoutExt123.cs
@@ -7,14 +7,47 @@ using log4net.Util;
 
 namespace log4net.ext123
 {
-	public class ShortLevelPatternConverter : PatternLayoutConverter
+	// sample 1: <layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level - %message' /></layout>
+	// sample 2: <layout type='log4net.ext123.PatternLayoutExt123'><conversionPattern value='%short-level{2} - %message' /></layout>
+	//
+	public class ShortLevelPatternConverter : PatternLayoutConverter, IOptionHandler
 	{
 		protected override void Convert(TextWriter writer, LoggingEvent loggingEvent) {
 			if (null == writer) throw new ArgumentNullException("writer");
 			if (null == loggingEvent) throw new ArgumentNullException("loggingEvent");
 
-			writer.Write(loggingEvent.Level.DisplayName[0]);
+			string levelDisplayName = loggingEvent.Level.DisplayName;
+
+			if (1 == myLevelNameLength)
+				writer.Write(levelDisplayName[0]);
+			else if (myLevelNameLength > 0)
+				writer.Write(levelDisplayName.Length < myLevelNameLength
+					? levelDisplayName.PadRight(myLevelNameLength)
+					: levelDisplayName.Substring(0, myLevelNameLength));
 		}
+
+		public void ActivateOptions()
+		{
+			myLevelNameLength = 1;
+			if (null == Option) return;
+			
+			string optStr = Option.Trim();
+			if (0 != optStr.Length) {
+				int lengthVal;
+				if (SystemInfo.TryParse(optStr, out lengthVal))
+				{
+					if (lengthVal < 0)
+						LogLog.Error(DeclaringType, "ShortLevelPatternConverter: Length option (" + optStr + ") isn't a positive integer.");
+					else
+						myLevelNameLength = lengthVal;
+				}
+				else
+					LogLog.Error(DeclaringType, "ShortLevelPatternConverter: Length option '" + optStr + "' not a decimal integer.");
+			}
+		}
+
+		private readonly static Type DeclaringType = typeof(ShortLevelPatternConverter);
+		private int myLevelNameLength = 1;
 	}
 
 	public class PatternLayoutExt123 : PatternLayout
