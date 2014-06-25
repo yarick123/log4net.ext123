4721c738909d9d344dd353256e6be25f13761764
yarick123 <yarick123@users.noreply.github.com>
2014-06-25 08:50:06 +0200

    throw ArgumentNullException if writer or loggingEvent argument is null

--- log4net.ext123/PatternLayoutExt123.cs
+++ log4net.ext123/PatternLayoutExt123.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using log4net.Core;
 using log4net.Layout;
@@ -6,10 +7,12 @@ using log4net.Util;
 
 namespace log4net.ext123
 {
-
 	public class ShortLevelPatternConverter : PatternLayoutConverter
 	{
 		protected override void Convert(TextWriter writer, LoggingEvent loggingEvent) {
+			if (null == writer) throw new ArgumentNullException("writer");
+			if (null == loggingEvent) throw new ArgumentNullException("loggingEvent");
+
 			writer.Write(loggingEvent.Level.DisplayName[0]);
 		}
 	}
